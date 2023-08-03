66d3e57d7dec43bb48f404163f774f4b2e9afad0
Chuột Bạch <thangcoiofficial@gmail.com>
2023-08-03 19:57:47 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const bodyParser = require('body-parser')
 const handlebars = require("express-handlebars");
 const cors = require('cors');
 const app = express();
