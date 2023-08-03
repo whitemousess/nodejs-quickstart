dfb1eeea6a84b2d6b6122e3fc4419f2325d74d44
Chuột Bạch <thangcoiofficial@gmail.com>
2023-08-03 19:56:13 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 const path = require("path");
 const express = require("express");
 const handlebars = require("express-handlebars");
+const cors = require('cors');
 const app = express();
 const port = 3000;
+const cookieParser = require("cookie-parser");
+
 
 const route = require("./routes");
 const db = require("./config/db");
 
+app.use(cors());
+// for parsing application/json
+app.use(bodyParser.json()); 
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
+// read token in cookie
+app.use(cookieParser());
 
-
-app.use(express.static(path.join(__dirname, "public")));
 app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set("views", path.join(__dirname, "views"));
+
 
 route(app);
 db.connect();
